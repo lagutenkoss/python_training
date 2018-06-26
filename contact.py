6c2410aa231ee0a54538771f869f374660d2b631
s.s.lagutenko <neto4ka84@mail.ru>
2018-06-26 17:55:06 +0300

    Добавление праметров в методы x1

---
+++ contact.py
@@ -0,0 +1,23 @@
+class Contact:
+
+    def __init__(self,first_name, middle_name, last_name, title, company, adress, telephone, mobile, work,
+                       fax, e_mail, mailtwo, mailthree, homepage, adress2, homeadress, notestwo, byear, ayear):
+        self.first_name=first_name
+        self.middle_name=middle_name
+        self.last_name=last_name
+        self.title=title
+        self.company=company
+        self.adress=adress
+        self.telephone=telephone
+        self.mobile=mobile
+        self.work=work
+        self.fax=fax
+        self.e_mail=e_mail
+        self.mailtwo=mailtwo
+        self.mailthree=mailthree
+        self.homepage=homepage
+        self.adress2=adress2
+        self.homeadress=homeadress
+        self.notestwo=notestwo
+        self.byear=byear
+        self.ayear=ayear
