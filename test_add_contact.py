42a0d95a588e505794a1f70ae391ee9d50c1a4bd
s.s.lagutenko <neto4ka84@mail.ru>
2018-06-27 08:47:59 +0300

    Добавление шаблона для контактов, исправление заполнения

--- test_add_contact.py
+++ test_add_contact.py
@@ -28,14 +28,114 @@ class test_add_contact(unittest.TestCase):
         self.return_to_home_page(wd)
         self.logout(wd)
 
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, wd, contact):
+        # fill_contact_form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.adress)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.telephone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.e_mail)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.mailtwo)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.mailthree)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[3]").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.adress2)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.homeadress)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notestwo)
+        # contact_creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+
+
+    def open_contact_page_to_add(self, wd):
+        wd.find_element_by_link_text("add new").click()
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/edit.php")
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+class test_add_empty_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+
     def test_add_empty_contact(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_contact_page_to_add(wd)
-        self.create_contact(wd, Contact(first_name="", middle_name="", last_name="", title="", company="", adress="", telephone="",
-                            mobile="", work="", fax="", e_mail="", mailtwo="", mailthree="", homepage="", adress2="",
-                            homeadress="", notestwo="", ayear="", byear=""))
+        self.create_contact(wd, Contact(first_name="", middle_name="", last_name="", title="", company="", adress="",
+                                        telephone="",
+                                        mobile="", work="", fax="", e_mail="", mailtwo="", mailthree="", homepage="",
+                                        adress2="",
+                                        homeadress="", notestwo="", ayear="", byear=""))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -89,17 +189,11 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
         wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[3]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[2]").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[2]").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(contact.byear)
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[3]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[3]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[2]").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[2]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys(contact.ayear)
@@ -114,8 +208,6 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("notes").send_keys(contact.notestwo)
         # contact_creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-
 
     def open_contact_page_to_add(self, wd):
         wd.find_element_by_link_text("add new").click()
@@ -134,7 +226,6 @@ class test_add_contact(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
-
 
 if __name__ == '__main__':
     unittest.main()
