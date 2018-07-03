d932dd8332b3ce9a9b4059d69b0abeddb36d8f2e
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-03 18:31:21 +0300

    Оптимизация ввода данных в формы -7 52

--- fixture/group.py
+++ fixture/group.py
@@ -12,7 +12,12 @@ class GroupHelper:
         self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
+        self.fill_group_form(group, wd)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def fill_group_form(self, group, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -22,9 +27,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
 
     def edit_first_group(self, group):
         wd = self.app.wd
@@ -34,15 +36,7 @@ class GroupHelper:
         # edit group creation
         wd.find_element_by_name("edit").click()
         # edit group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group, wd)
         # update group creation
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
@@ -50,14 +44,24 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+
+    def select_first_group(self):
+        wd.find_element_by_name("selected[]").click()
 
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-
+    def modify_first_group_header(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
