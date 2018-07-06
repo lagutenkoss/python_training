14008fe5074ceb935926aea8cfd8a3c77031ca12
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-06 21:20:56 +0300

    "интеллектуальность" удаления контактов и групп

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,3 +1,8 @@
+from model.contact import Contact
+
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.delete_first_contact()
+
