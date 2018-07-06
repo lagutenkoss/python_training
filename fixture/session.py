ca04c3397c02007e5ec5ad766acf38fc0545b45b
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-06 13:47:32 +0300

    "интеллектуальность" функций login и logout: проверка текущего состояния сессии

--- fixture/session.py
+++ fixture/session.py
@@ -20,17 +20,21 @@ class SessionHelper:
 
     def ensure_logout(self):
         wd = self.app.wd
-        if self.is_logget_in():
+        if self.is_logged_in():
             self.logout()
 
-    def is_logget_in(self):
+    def is_logged_in(self):
         wd = self.app.wd
         return len(wd.find_elements_by_link_text("Logout")) > 0
 
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div[1]/div[1]/form/b").text == "("+username+")"
+
     def ensure_login(self, username, password):
         wd = self.app.wd
-        if self.is_logget_in():
-            if self.is_logget_in_as(username):
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
                 return
             else:
                 self.logout()
