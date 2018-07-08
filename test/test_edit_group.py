a92d934ba5bf261a88e3ce9b8bbb8c12e32bc802
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-08 12:45:29 +0300

    "интеллектуальность" удаления, модификации и редактирования контактов и групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,4 +2,7 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="Group name", header="Group header", footer="Group footer"))
     app.group.edit_first_group(Group(name="edit group name", header="edit group header", footer="edit group footer"))
+
