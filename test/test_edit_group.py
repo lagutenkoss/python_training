0582bf2ca7b524aed55935d425de6756de42e3e7
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-10 18:07:55 +0300

    список для добавлениz,удаления,модификации групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,5 +4,8 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group name", header="Group header", footer="Group footer"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="edit group name", header="edit group header", footer="edit group footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
