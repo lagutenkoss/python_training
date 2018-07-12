53647547de6a448d94388ecd16db929f83ef36ad
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-12 15:59:33 +0300

    Реализованы проверки в виде сравнения списков для групп и контактов

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -6,8 +6,15 @@ def test_add_contact(app):
         app.contact.create(Contact(first_name="First name", middle_name="Middle name", last_name="Last name", title="Title", company="Company", adress="adress", telephone="Telephone",
                                      mobile="Mobile", work="Work", fax="Fax", e_mail="E-mail", mailtwo="E-mail2", mailthree="E-mail3", homepage="Homepage", adress2="adress2",
                                      homeadress="Homeadress", notestwo="notes2", ayear="1995", byear="1995"))
-    app.contact.edit_first_contact(Contact(first_name="edit first name", middle_name="edit middle name", last_name="edit last name", title="edit title", company="edit company", adress="edit adress", telephone="edit telephone",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(first_name="edit first name", middle_name="edit middle name", last_name="edit last name", title="edit title", company="edit company", adress="edit adress", telephone="edit telephone",
                                      mobile="edit mobile", work="edit work", fax="edit fax", e_mail="edit e-mail", mailtwo="edit e-mail2", mailthree="edit e-mail3", homepage="edit homepage", adress2="edit adress2",
-                                     homeadress="edit home adress", notestwo="edit notes2", ayear="2000", byear="2000"))
+                                     homeadress="edit home adress", notestwo="edit notes2", ayear="2000", byear="2000")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,7 +5,11 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group name", header="Group header", footer="Group footer"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="edit group name", header="edit group header", footer="edit group footer"))
+    group = Group(name="edit group name", header="edit group header", footer="edit group footer")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
