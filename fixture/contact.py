ab92c6fcb8fd13e332008c2b67a06073282c740f
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-16 09:22:40 +0300

    случайность редактирования, модификации и удаления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -70,10 +70,13 @@ class ContactHelper:
         self.change_field_value("phone2", contact.homeadress)
         self.change_field_value("notes", contact.notestwo)
 
-    def edit_first_contact(self, contact):
+    def edit_first_contact(self):
+        self.edit_contact_by_index(0)
+
+    def edit_contact_by_index(self, index, contact):
         wd = self.app.wd
         self.return_to_home()
-        self.click_for_edit_first_contact()
+        self.click_for_edit_contact_by_index(index)
         # edit_contact_form
         self.fill_contact_form(contact)
         wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[11]").click()
@@ -85,26 +88,36 @@ class ContactHelper:
         self.return_to_home_page()
         self.contact_cache = None
 
-    def click_for_edit_first_contact(self):
+    def click_for_edit_contact_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_xpath("//div[1]/div[4]/form[2]/table/tbody/tr[2]/td[8]/a/img").click()
+        wd.find_elements_by_xpath("//div[1]/div[4]/form[2]/table/tbody/tr/td[8]/a/img")[index].click()
 
     def delete_first_contact(self):
+        self.delete_contact_by_index(0)
+
+    def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.return_to_home()
         # select first contact
-        wd.find_element_by_name("selected[]").click()
+        self.select_contact_by_index(index)
         # submit deletion
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
         self.return_to_home()
         self.contact_cache = None
 
-    def modify_first_contact(self, new_contact_data):
+    def select_contact_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def modify_first_contact(self):
+        self.modify_contact__by_index(0)
+
+    def modify_contact_by_index(self, index, new_contact_data):
         wd = self.app.wd
         self.return_to_home()
         # modify first contact
-        self.click_for_edit_first_contact()
+        self.click_for_edit_contact_by_index(index)
         # fill group form
         self.fill_contact_form(new_contact_data)
         # submit modification
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
+from random import randrange
 
 
-def test_modify_contact_first_name(app):
+def test_modify_some_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="First name", middle_name="Middle name", last_name="Last name", title="Title", company="Company", adress="adress", telephone="Telephone",
                                      mobile="Mobile", work="Work", fax="Fax", e_mail="E-mail", mailtwo="E-mail2", mailthree="E-mail3", homepage="Homepage", adress2="adress2",
                                      homeadress="Homeadress", notestwo="notes2", ayear="1995", byear="1995"))
     old_contacts = app.contact.get_contact_list()
+    index = randrange(len(old_contacts))
     contact = Contact(first_name="New First name")
-    contact.id = old_contacts[0].id
-    app.contact.modify_first_contact(contact)
+    contact.id = old_contacts[index].id
+    app.contact.modify_contact_by_index(index, contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts[0] = contact
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
