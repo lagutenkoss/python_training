eec8b7759a3949c73b7c16fe8a9ab41978fa7dbf
s.s.lagutenko <neto4ka84@mail.ru>
2018-07-20 16:38:16 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -8,12 +8,15 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption('--browser')
+    baseUrl = request.config.getoption('--baseUrl')
+    password = request.config.getoption('--password')
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, baseUrl=baseUrl)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, baseUrl=baseUrl)
+    fixture.session.ensure_login(username="admin", password=password)
     return fixture
 
 
@@ -24,3 +27,9 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='firefox')
+    parser.addoption('--baseUrl', action='store', default="http://localhost/addressbook/")
+    parser.addoption('--password', action='store', default='secret')
--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,7 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+#для фокса
+#from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
+
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -6,11 +9,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser, baseUrl):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError('Unrecognized browser %s' % browser)
+#       self.wd = WebDriver(capabilities={"marionette": False})
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.baseUrl = baseUrl
 
     def is_valid(self):
         try:
@@ -22,7 +34,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("http://localhost/addressbook/") and len(wd.find_elements_by_xpath("//div[@id='header']/a/img")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.baseUrl)
 
     def destroy(self):
         self.wd.quit()
