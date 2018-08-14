cc787d1dfad2e4bd863e50255adc06caecfcab73
Александр <neto4ka84@mail.ru>
2018-08-14 21:48:47 +0300

    Ограничения или условия отбора загружаемых данных для контакта

---
+++ look_db_connection_group.py
@@ -0,0 +1,12 @@
+from fixture.db import DbFixture
+
+db = DbFixture(host='127.0.0.1', name='addressbook', user='root', password='')
+
+try:
+    groups = db.get_group_list()
+    for group in groups:
+        print(group)
+    print(len(groups))
+finally:
+    db.destroy()
+
